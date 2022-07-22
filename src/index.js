041475f8f6f60eae01a199d4270d808bdd647472
Wilfreed Audouin <wilfreedaudouin@gmail.com>
2022-07-23 00:08:15 +0200

    correcting routes

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ root.render(
   <React.StrictMode>
 		<Router>
 			<Routes>
-				<Route path="/" element={<App/>} />
+				<Route path="" element={<App/>} />
 				<Route path=":CCA"element={<Details/>} />
 			</Routes>
     </Router>
